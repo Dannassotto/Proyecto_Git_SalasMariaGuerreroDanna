7cd25430c6953150647c25f06adad14f589a382b
Dannassotto <valentinaguerrero0810@gmail.com>
2024-08-16 15:38:08 -0500

    feat: :sparkles: Menu principal
    se creo el menu principal del proyecto a realizar

---
+++ main.py
@@ -0,0 +1,11 @@
+
+def menu_principal():
+    while True:
+        print("*********")
+        print("* MENU  *")
+        print("*********")
+        print("1. Resgistrar sucursal")
+        print("2. Editar sucursal")
+        print("3. Eliminar sucursal")
+        
+        opcion=input ("seleccione una opcion (1-3):")
