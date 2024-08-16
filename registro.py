d491e889e2739c6cba3017402e781af58af03b04
maríaHelenaSalas <mariahelenasalas83@gmail.com>
2024-08-16 16:17:25 -0500

    registro 2

---
+++ registro.py
@@ -0,0 +1,18 @@
+import os
+import json
+def newRegistro ()
+    
+    title = """                         
+*********************************
+* sucurlal                      *
+*********************************
+"""
+gf.borrar_pantalla()
+print(title)
+nombre_de_la_sucursal = input("ingrese el nombre de la sucursal")
+Direccion_Completa= ("ingrese la direccion")
+telefono= ("ingrese el numero de telefono")
+ID_del_gerente= ("ingrese el ID")
+
+
+
